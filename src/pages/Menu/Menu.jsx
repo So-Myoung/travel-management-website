4b78fad86280a748434b744f380d1ebbdd50c198
SO-HEE-722 <127752923+SO-HEE-722@users.noreply.github.com>
2023-05-09 22:16:15 +0900

    메뉴 메인 페이지

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import  {Container,Row,Col}  from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js';
+import './Menu.css';
 
 export default function Menu() {
-  return <div>MENU 페이지<br/>ITEM 상세 조회 까지만, 장바구니 기능 X</div>;
+  let [coffee] = useState(data);
+
+  return (
+    <Container>
+    <Row>
+          {
+            coffee.map(function(a,i){
+              return (
+                <Product className='coffee' img ={coffee[i].img} title = {coffee[i].title} content = {coffee[i].content} price={coffee[i].price}>
+                </Product>
+              )
+            })
+            
+          }
+      </Row>
+    </Container>
+
+  );
+}
+
+function Product(props){
+  console.log(props.img);
+  return (
+    <div className='coffee'>
+    <Col sm>
+            <img className='image' src ={props.img} width="200px" height="100px" style={{paddingTop: '30px'}}/>
+            <h4 className='orange'>{props.title}</h4>
+            <p>{props.content}</p>
+            <p>{props.price}</p>
+    </Col>
+    </div>
+  )
+  
 }
---
+++ src/pages/Menu/data.js
@@ -0,0 +1,50 @@
+let data = [
+    {
+      id : 0,
+      img : '../images/coffee.jpg',
+      title : "에스프레소",
+      content : "설명을 쓰세요",
+      price : "1800원"
+    },
+  
+    {
+      id : 1,
+      img : '../images/coffee.jpg',
+      title : "아메리카노",
+      content : "설명을 쓰세요",
+      price : "2800원"
+    },
+  
+    {
+      id : 2,
+      img : '../images/coffee.jpg',
+      title : "카페라떼",
+      content : "설명을 쓰세요",
+      price : "3500원"
+    },
+
+    {
+        id : 3,
+        img : '../images/coffee.jpg',
+        title : "모카라떼",
+        content : "설명을 쓰세요",
+        price : "3500원"
+      },
+
+      {
+        id : 4,
+        img : '../images/coffee.jpg',
+        title : "돌체라떼",
+        content : "설명을 쓰세요",
+        price : "3800원"
+      },
+      {
+        id : 5,
+        img : '../images/coffee.jpg',
+        title : "바닐라라떼",
+        content : "설명을 쓰세요",
+        price : "3500원"
+      }
+  ]
+  
+  export default data;
